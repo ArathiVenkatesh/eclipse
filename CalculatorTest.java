345f1a2679e46c005d1c5d1b82ee10a9d0b670c2
ArathiVenkatesh <arathiv768@gmail.com>
2026-04-23 02:54:05 +0530

    Create CalculatorTest.java

---
+++ CalculatorTest.java
@@ -0,0 +1,31 @@
+package calculatorexample; 
+ 
+import static org.junit.jupiter.api.Assertions.*; 
+import org.junit.jupiter.api.Test; 
+ 
+class CalculatorTest { 
+ 
+    @Test 
+    void testAdd() { 
+        Calculator calc = new Calculator(); 
+        assertEquals(6, calc.add(2, 4)); 
+    } 
+ 
+    @Test 
+    void testSubtract() { 
+        Calculator calc = new Calculator(); 
+        assertEquals(2, calc.subtract(5, 3)); 
+    } 
+ 
+    @Test 
+    void testMultiply() { 
+        Calculator calc = new Calculator(); 
+        assertEquals(6, calc.multiply(2, 3)); 
+    } 
+ 
+    @Test 
+    void testDivide() { 
+        Calculator calc = new Calculator(); 
+        assertEquals(2, calc.divide(4, 2)); 
+    } 
+} 
